7ff93dbdbddef6b5850c221312ab8adf44e66987
Tallulah Carlisle <tallulah.carlisle@gmail.com>
2021-01-10 23:15:57 +0000

    First commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = '85CQEGr1g4RlOZ3j07B0B44ggR15FbTzuRJzgta77x1DUohAw2z3wWycO8f5Z382LXZDtvXZwFUa297vYKitFJCnrqDFTaelazCDI5CZ34pNheR0V1ljanPyOlxsX3Yx'; // Insert API key here.
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+       {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      console.log(response);
+      return response.json();
+    }).then(jsonResponse => {
+      console.log(jsonResponse);
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
